220c15f0117a9d42553f3b85e168b11c9606af54
Mimoun Majd <majdmn6@gmail.com>
2019-08-31 11:00:01 +0100

    Update Utils.js

--- Config/Utils.js
+++ Config/Utils.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321'
+    'secretKey': '12345-67890-09876-54321',
+    'URI' : "redis://h:pf1da4fe092bf37b9e5ee5ba349b01e307364c75c47a345ed57e64a6227761b09@ec2-3-248-19-151.eu-west-1.compute.amazonaws.com:18339"
 }
